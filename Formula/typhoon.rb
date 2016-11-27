75838b611906e9540f222e25cc6bc13392840b54
Aleksey Garbarev <aleksey@appsquick.ly>
2016-11-28 03:39:42 +0600

    typhoon: 0.0.1

--- Formula/typhoon.rb
+++ Formula/typhoon.rb
@@ -1,8 +1,8 @@
 class Typhoon < Formula
   desc "Typhoon DI framework for Swift"
   homepage "http://typhoonframework.org"
-  url "https://github.com/appsquickly/TyphoonSwift/archive/0.0.1.tar.gz"
-  sha256 "dcb53c3ff6e214b48039e051c1ce16cadf2acb155ab7d1dd617db7d2b8dca05f"
+  url "https://github.com/appsquickly/TyphoonSwift/archive/0.0.1.zip"
+  sha256 "7e1d2312fa5bd4335582a4eacad98f759a6b5f035c837a0b60bb2883e0aae8da"
   head "https://github.com/appsquickly/TyphoonSwift.git"
 
   depends_on :xcode => ["8.1", :build]
